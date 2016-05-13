941b141ab458bbed96cd7ab23e745e2fa105b55c
Derfador1 <Derfador1@gmail.com>
2016-05-13 15:47:22 -0400

    added comments to my dispatcher

--- dispatcher.c
+++ dispatcher.c
@@ -41,36 +41,42 @@ int main(void)
 	FD_ZERO(&master);
 	FD_ZERO(&read_fds);
 
-	for(int a = 0; a < 256; a++) {
+	//my own preference for initializing buffer
+	for(int a = 0; a < MAXN; a++) {
 		buffer[a] = '\0';
 	}
 
 	printf("Setting up...\n");
 	
+	//sets up my socket using af unix
 	sock = socket(AF_UNIX, SOCK_STREAM, 0);
 	if(sock < 0) {
 		perror("socket");
 		exit(1);
 	}
 
+	//setting the address im using to be reusable for multiple connections
 	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) < 0) {
 		perror("setsockopt(SO_REUSEADDR) failed");
 	}
 
+	//setting up things for binding
 	local.sun_family = AF_UNIX;
 	strcpy(local.sun_path, SOCK_PATH);
 	unlink(local.sun_path);
 	len = strlen(local.sun_path) + sizeof(local.sun_family);
 
+	//binds socket
 	if(bind(sock, (struct sockaddr *)&local, len) == -1) {
 		perror("binding");
 		close(sock);	
 		exit(1);
 	}
 
+	//chmod the file so many users can operate listener
 	chmod(SOCK_PATH, 0777);
 
-	if (listen(sock, 5) == -1) {
+	if (listen(sock, 5) == -1) { //listen for 5 connections
 		perror("listen");
 		exit(1);
 	}
@@ -80,6 +86,7 @@ int main(void)
 	https://github.com/Ragnok/relay/blob/master/dispatcher.c
 	*/
 
+	//these 2 are used to hold all my socket descriptors
 	FD_SET(sock, &master);
 	FD_SET(STDIN_FILENO, &master);
 
@@ -89,6 +96,7 @@ int main(void)
 	fflush(stdout);
 
 	while(1) {
+		//goes through all of the socket descriptors 
 		read_fds = master;
 		if(select(max + 1, &read_fds, NULL, NULL, NULL) == -1) {
 			perror("select");
@@ -98,6 +106,7 @@ int main(void)
 		int current;
 		for (current = 0; current <= max; current++) {
 			if(FD_ISSET(current, &read_fds)) {
+				//used for new connections
 				if(current == sock) {
 					addrlen = sizeof(remote1);
 					accept_s = accept(sock, (struct sockaddr *)&remote1, &addrlen);
@@ -112,14 +121,18 @@ int main(void)
 						}
 					}
 				}
+				//used for existing connections
 				else if(current == STDIN_FILENO){
+					//reads in from stdin 
 					if ((bytes = read(current, buffer, sizeof(buffer))) > 0) {
+						//eot check for q
 						if (strncmp(buffer, "q\n", 2) == 0) {
 							printf("quitting..\n");
 							close(sock);
 							exit(1);
 						}
 
+						//to send to each connection
 						for(current = 0; current <= max; current++) {
 							if (FD_ISSET (current, &master) && current != STDIN_FILENO && current != sock) {
 								if (send(current, buffer, bytes, 0) == -1) {
